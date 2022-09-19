ae9b77479c51e6663a2a0c53d33c8120625edc36
Martin John <20387863+martingjohn@users.noreply.github.com>
2022-09-19 17:28:31 +0100

    making progress

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,9 @@ ARG FROM_VER
 FROM alpine:${FROM_VER:-3.16.2}
 
 RUN apk add --no-cache \
+        bash \
         postfix
 
+WORKDIR /app
+COPY entrypoint.sh /app/entrypoint.sh
 CMD sleep inf
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,21 @@
+#!/bin/bash
+
+IFS=$(echo -en "\n\b")
+for i in $(env | grep POSTCONF_ | sort)
+do
+	i=${i#POSTCONF_}
+	#echo "$i"
+	echo postconf -e $i
+	postconf -e $i
+done
+
+if [[ "x$POSTFIX_SASL_PASSWD" -ne "x" ]]
+then
+	cp ${POSTFIX_SASL_PASSWD} /etc/postfix/sasl_passwd
+	chown root:root /etc/postfix/sasl_passwd
+	chmod 600 /etc/postfix/sasl_passwd
+	postmap /etc/postfix/sasl_passwd
+fi
 
 
-
-postfix start-fg
+#postfix start-fg
